0055923040b7969f7ad823320cb2ed617490622d
thomascwei <thomas.c.wei@gmail.com>
2022-04-29 09:40:41 +0800

    .

--- deadlocks_train/common/model.go
+++ deadlocks_train/common/model.go
@@ -3,18 +3,23 @@ package common
 import "sync"
 
 type Train struct {
-	Id          int
+	Id int
+	// 火車長度
 	TrainLength int
-	Front       int
+	// 車頭位置
+	Front int
 }
 
+// Intersection 火車與交岔點的互動
 type Intersection struct {
 	Id       int
 	Mutex    sync.Mutex
 	LockedBy int
 }
 
+// Crossing 交岔點
 type Crossing struct {
+	// 某條軌道起點到交岔點的距離,也就是交岔點的位置
 	Position     int
 	Intersection *Intersection
 }
--- deadlocks_train/deadlock/train.go
+++ deadlocks_train/deadlock/train.go
@@ -5,15 +5,24 @@ import (
 	"time"
 )
 
+// MoveTrain distance代表火車需要前進的距離,crossings代表將會經過過的交岔路口列表
 func MoveTrain(train *Train, distance int, crossings []*Crossing) {
+	//在火車到達目的地前持續移動火車
 	for train.Front < distance {
+		//每次移動1
 		train.Front += 1
+		//檢查我們是否在一個路口上
 		for _, crossing := range crossings {
+			//剛好在路口
 			if train.Front == crossing.Position {
+				//取得鎖
 				crossing.Intersection.Mutex.Lock()
+				//表明該路口被哪一台火車占用
 				crossing.Intersection.LockedBy = train.Id
 			}
+			//車尾
 			back := train.Front - train.TrainLength
+			//判斷是否通過交岔點
 			if back == crossing.Position {
 				crossing.Intersection.LockedBy = -1
 				crossing.Intersection.Mutex.Unlock()
